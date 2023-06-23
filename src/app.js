f46665f35ed844f712a28711f46a5d5daf25223a
Alejandro Carrillo <a.x.c.m.2002@gmail.com>
2023-06-22 21:41:02 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+  var pronoun = ['the','our'];
+  var adj = ['great', 'big' ];
+  var noun = ['jogger','racoon'];
+
+  for (i = 0; i < pronoun.length; i++) {
+    for (b = 0; b < adj.length; b++){
+      for (x = 0; x < noun.length; x++){
+
+        console.log(pronoun[i] + adj[b] + noun[x] + ".com");
+      }
+      
+    }
+  };
+
